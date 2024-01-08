859ce2342135895dde7fcb7e212083cff25f34aa
Yusuf <yusufbilalctn@gmail.com>
2024-01-08 16:52:20 +0300

    Week18 Implement synchonized access in getDriver method for thread-safe WebDriver management

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -10,6 +10,7 @@ public abstract class BasePage {
 
 	WebDriver driver = Driver.getDriver();
 
+
 	public BasePage() {
 		PageFactory.initElements(driver, this);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
